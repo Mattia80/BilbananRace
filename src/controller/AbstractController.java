89fcffbd633dc9dda6b5909689a73e28a568616a
Mattia Cavalieri Manasse <cavalierimanasse.2010869@strudenti.uniroma1.it>
2025-06-12 18:41:14 +0200

    Creation of GameManager

--- src/controller/AbstractController.java
+++ src/controller/AbstractController.java
@@ -1,5 +1,30 @@
 package controller;
 
-public abstract class AbstractController {
+import model.AbstractModel;
+import view.AbstractView;
 
+public abstract class AbstractController {
+    protected AbstractModel model;
+    protected AbstractView view;
+
+    public AbstractController(AbstractModel model, AbstractView view) {
+        this.model = model;
+        this.view = view;
+    }
+
+    public AbstractModel getModel() {
+        return model;
+    }
+
+    public void setModel(AbstractModel model) {
+        this.model = model;
+    }
+
+    public AbstractView getView() {
+        return view;
+    }
+
+    public void setView(AbstractView view) {
+        this.view = view;
+    }
 }
---
+++ src/controller/GameManager.java
@@ -0,0 +1,32 @@
+package controller;
+
+import view.GamePanel;
+import view.GameWindow;
+
+public class GameManager {
+    private static GameManager instance;
+    private final GamePanel gamePanel;
+    private GameWindow gameWindow;
+
+    public static GameManager getInstance() {
+        if (instance == null) {
+            instance = new GameManager();
+        }
+        return instance;
+    }
+
+    private GameManager() {
+        this.gamePanel = new GamePanel();
+        this.gameWindow = new GameWindow(this.gamePanel);
+        this.gamePanel.setFocusable(true);
+        this.gamePanel.requestFocus();
+    }
+
+    public GamePanel getGamePanel() {
+        return gamePanel;
+    }
+
+    public GameWindow getGameWindow() {
+        return gameWindow;
+    }
+}
